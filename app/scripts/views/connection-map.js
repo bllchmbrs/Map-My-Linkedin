0ba08fd1f762e5bbcb6a4f36e661b01a9f110501
anabranch <wac.chambers@gmail.com>
2014-07-24 19:48:19 -0700

    removed unnecessary code

--- app/scripts/views/connection-map.js
+++ app/scripts/views/connection-map.js
@@ -9,24 +9,18 @@ define([
     'use strict';
 
     var ConnectionMapView = Backbone.View.extend({
-        template: JST['app/scripts/templates/connection-map.ejs'],
-
-        tagName: 'div',
-
-        id: '',
-
-        className: '',
-
-        events: {
-
-        },
-
         initialize: function () {
-            this.listenTo(this.model, 'change', this.render);
+            this.render();
         },
 
         render: function () {
-            this.$el.html(this.template(this.model.toJSON()));
+            console.log(this.model);
+            this.marker = new google.maps.Marker({
+                position: this.model.get("geocodedLocation"),
+                map: this.model.get("map"),
+                title: "Hello"
+            });
+            console.log(this.marker);
         }
     });
 
