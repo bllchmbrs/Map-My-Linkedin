1e3d0b3faa788198e1916552f915e28ddfec11f1
anabranch <wac.chambers@gmail.com>
2014-07-24 19:47:57 -0700

    added map model

---
+++ app/scripts/models/map.js
@@ -0,0 +1,12 @@
+/*global define*/
+
+define(function(require){
+    'use strict';
+    var map = new google.maps.Map(document.getElementById("map-location"),
+    {
+        zoom:3,
+        center: new google.maps.LatLng(39.742043, -104.991531)
+    });
+
+    return map;
+});
