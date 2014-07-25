411a05bd388e710d748a40c6771d0fe8074faec1
anabranch <wac.chambers@gmail.com>
2014-07-24 19:47:40 -0700

    parsing connection values + dependencies

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,8 +12,43 @@ require.config({
 });
 
 define(function(require){
-    var util = require('./helper/util');
+    var ConnectionModel = require('./models/connection');
+    var ConnectionCollection = require('./collections/connection');
+    var ConnectionMapView = require('./views/connection-map');
+    var Util = require('./helper/util');
     var IN = window.IN;
+    var connections = new ConnectionCollection;
+    var globalMap = require('./models/map');
 
-    IN.Event.on(IN, 'auth', util.getConnections);
+    function getConnections () {
+        IN.API.Connections('me')
+        .fields('firstName', 'lastName', 'industry', 'location',
+            'picture-url', 'positions', 'num-connections', 'num-connections-capped',
+            'formatted-name')
+        .result(parseConnectionValues);
+    }
+
+    function parseConnectionValues (unParsedConnections) {
+        var values = unParsedConnections.values;
+        for (var i = 0; i < values.length; i++) {
+            var cleanedLocation = Util.cleanLocation(values[i].location);
+            var createdModel = new ConnectionModel({
+                    name: values[i].formattedName,
+                    industry: values[i].industry,
+                    location: cleanedLocation.name,
+                    // location: cleanedLocation.name,
+                    // geocodedLocation: cleanedLocation.geocoded,
+                    geocodedLocation: new google.maps.LatLng(37.733795, -122.446747),
+                    map: globalMap
+            });
+            connections.add(
+                createdModel
+            );
+            new ConnectionMapView({
+                model: createdModel
+            })
+        }
+    }
+
+    IN.Event.on(IN, 'auth', getConnections);
 });
