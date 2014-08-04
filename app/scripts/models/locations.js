ec5bda29090c579e453497e420887ccc0f9fd7cb
anabranch <wac.chambers@gmail.com>
2014-08-04 16:53:59 -0700

    Added Locations Singleton

---
+++ app/scripts/models/locations.js
@@ -0,0 +1,60 @@
+/*global define*/
+
+define(['underscore'], function(_) {
+    'use strict';
+    var Locations = function () {
+
+        var _locations = [];
+        // example: {
+        //     originalCountryCode: 'us',
+        //     originalName: 'San Francisco Bay Area', 
+        //     geocodedUrl: http://.....
+        //     geocodedData: {
+        //         data: is_here
+        //     }
+        // }
+
+        function generateGeocoded (name){
+            var first = 'https://maps.googleapis.com/maps/api/geocode/json?';
+            var key = 'key=AIzaSyD9HhZRFpmhfYuEgkfmkE-5ucBnEE8rZzo&';
+            var nameAsList = name.split(' ');
+            delete nameAsList[nameAsList.indexOf('Greater')];
+            delete nameAsList[nameAsList.indexOf('Area')];
+            var address = 'address=' + encodeURIComponent(nameAsList.join(' '));
+            return first + key + address;
+        }
+
+        return {
+            addOrGetByLocation: function (linkedinlocation){
+                console.log('adding to locations...');
+                if (!linkedinlocation) {
+                    return []; // if no location ignore
+                }
+
+                var countryCode = linkedinlocation.country.code;
+                var locationName = linkedinlocation.name;
+
+                if (_.where(_locations, {
+                    originalName: locationName,
+                    originalCountryCode: countryCode // Testing for an empty array in javascript is stupid...
+                })[0] === undefined) {
+                    console.log('pushing...');
+                    _locations.push({
+                        originalName: locationName,
+                        originalCountryCode: countryCode,
+                        geocodedUrl: generateGeocoded(locationName),
+                    });
+                }
+                return _.where(_locations,{
+                    originalName: locationName,
+                    originalCountryCode: countryCode
+                })[0];
+            },
+            getAll: function () {
+                return _locations;
+            }
+        };
+    };
+
+    return new Locations;
+});
