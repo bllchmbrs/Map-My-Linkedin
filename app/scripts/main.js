05f9e525c5367dda61f58a9bcca2aeb28e056eeb
anabranch <wac.chambers@gmail.com>
2014-08-06 16:25:47 -0700

    removed locations, code cleanup

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,9 +14,7 @@ require.config({
 define(function(require){
     // Models
     var ConnectionModel = require('./models/connection');
-    var Locations = require('./models/locations');
     var googleMaps = require('./models/gmapshelper');
-    var globalMap = googleMaps.map;
 
     // Collections
     var ConnectionCollection = require('./collections/connection');
@@ -24,9 +22,15 @@ define(function(require){
     // Views
     var ConnectionMapView = require('./views/connection-map');
 
+    //Helpers
+    var FunctionHelpers = require('./utils/functions');
+
     // Instantiations
     var IN = window.IN;
     var connections = new ConnectionCollection;
+    var globalMap = googleMaps.map;
+    var geocodeAddress = googleMaps.geocodeAddress;
+    var cleanLocationName = FunctionHelpers.cleanLocationName;
 
     function getConnections () {
         console.log('getting connections');
@@ -38,16 +42,20 @@ define(function(require){
     }
 
     function parseConnectionValues (unParsedConnections) {
+        console.log('Now parsing raw LinkedIn connections...');
         var values = unParsedConnections.values;
-        console.log('Now parsing connection values...');
+
         for (var i = 0; i < values.length; i++) {
-            Locations.addOrGetByLocation(values[i].location);
-            var newConnectionModel = new ConnectionModel({
-                    name: values[i].formattedName,
-                    industry: values[i].industry,
-                    linkedinLocation: values[i].location ? values[i].location : {}
-                });
-            connections.add(newConnectionModel);
+            // need to check if it's the collection here
+            console.log(values[i]);
+            // if not
+            new ConnectionModel({
+                linkedLocationName: values[i].location ? values[i].location.name : "",
+                linkedinlocationCountryCode: values[i].location ? values[i].location.country.code : "",
+                displayLocationName: values[i].location ? cleanLocationName(values[i].location.name) : "",
+                people: [values[i]],
+                geocodedLocationData: values[i].location ? geocodeAddress(values[i].location.name) : ""
+            })
         }
     }
 
