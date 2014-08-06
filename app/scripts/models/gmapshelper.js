d4479c56528f6923eb018642d4b50d4ebd6c21e9
anabranch <wac.chambers@gmail.com>
2014-08-06 16:25:26 -0700

    added in geocoding address to gmaps helpers

--- app/scripts/models/gmapshelper.js
+++ app/scripts/models/gmapshelper.js
@@ -9,10 +9,24 @@ define(function(){
     });
     var geocoder = new google.maps.Geocoder();
 
+    function geocodeAddress (address) {
+        // Need a way of slowing this down...
+
+        // geocoder.geocode({'address': address}, function (results, status){
+        //      if (status == google.maps.GeocoderStatus.OK) {
+        //         console.log(results);
+        //      }
+        //      else {
+        //         console.log(status);
+        //      }
+        // });
+        return "Geo data"
+    }
 
 
     return {
         map: map,
-        geocoder: geocoder
+        geocoder: geocoder,
+        geocodeAddress: geocodeAddress
     };
 });
