d1abf3e149029b47bdf39dd1d112f69f9454a505
anabranch <wac.chambers@gmail.com>
2014-08-07 22:10:12 -0700

    Added in geocoding

--- app/scripts/models/connection.js
+++ app/scripts/models/connection.js
@@ -2,18 +2,28 @@
 
 define([
     'underscore',
-    'backbone'
-], function (_, Backbone) {
+    'backbone',
+    './gmapshelper'
+], function (_, Backbone, googleMaps) {
     'use strict';
+
+    var geocodeAddress = googleMaps.geocodeAddress;
 
     var ConnectionModel = Backbone.Model.extend({
         initialize: function() {
         },
 
+        geocode: function() {
+            if (this.get('locationName') == ""){
+                console.log("-----");
+                console.log(this.get("locationName"));
+                console.log(this);
+            }
+            // this.set({geocodedLocationData : geocodeAddress(this.locationName)});
+        },
+
         defaults: {
-            linkedinLocationName: '',
-            linkedinLocationCountryCode: '',
-            displayLocationName: '',
+            locationName: '',
             people: [],
             geocodedLocationData: {}
         }
