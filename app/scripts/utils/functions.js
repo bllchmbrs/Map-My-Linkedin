9d9f8196d87dc5ec9fd4e5eff3f32b68dade50d3
anabranch <wac.chambers@gmail.com>
2014-08-07 22:12:04 -0700

    formatting

--- app/scripts/utils/functions.js
+++ app/scripts/utils/functions.js
@@ -1,39 +1,39 @@
 /*global define*/
 
-define(function(require){
+define(function(){
     'use strict';
     
     function cleanLinkedinLocation (rawlocationObject) {
-            var name = rawlocationObject ? rawlocationObject.name : undefined;
-            var nameAsList, address;
+        var name = rawlocationObject ? rawlocationObject.name : undefined;
+        var nameAsList, address;
 
-            if (name !== undefined) {
-                nameAsList = name.split(' ');
-                delete nameAsList[nameAsList.indexOf('Greater')];
-                delete nameAsList[nameAsList.indexOf('Area')];
-                address = nameAsList.join(' ');
-            }
-            
-            return address;
+        if (name !== undefined) {
+            nameAsList = name.split(' ');
+            delete nameAsList[nameAsList.indexOf('Greater')];
+            delete nameAsList[nameAsList.indexOf('Area')];
+            address = nameAsList.join(' ');
         }
+        
+        return address;
+    }
 
     function cleanLinkedinConnection (rawConnectionObject) {
         // we'll do all of our cleaning in here, will allow us to easily create people
+        var raw = rawConnectionObject;
+        var person = {};
+        
+        person.firstName = raw.firstName;
+        person.lastName = raw.lastName;
+        person.formattedName = raw.formattedName;
+        person.industry = raw.industry;
+        person.numConnections = raw.numConnections;
+        person.numConnectionsCapped = raw.numConnectionsCapped;
+        person.pictureUrl = raw.pictureUrl;
+        person.positions = raw.positions;
 
-            //         person = {
-            //     firstName: values[i].firstName,
-            //     lastName: values[i].lastName,
-            //     formattedName: values[i].formattedName,
-            //     industry: values[i].industry,
-            //     numConnections: values[i].numConnections,
-            //     numConnectionsCapped: values[i].numConnectionsCapped,
-            //     pictureUrl: values[i].pictureUrl,
-            //     positions: values[i].positions
-            // }
-
-            // console.log(person);
-        return {};
+        return person;
     }
+    
     return {
         cleanLinkedinLocation: cleanLinkedinLocation,
         cleanLinkedinConnection: cleanLinkedinConnection
