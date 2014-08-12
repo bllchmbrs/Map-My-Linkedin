5766fe27dc517f59d107c8d03815f7c496544605
anabranch <wac.chambers@gmail.com>
2014-08-12 12:24:51 -0700

    initial commit on new views

---
+++ app/scripts/views/featuredConnection.js
@@ -0,0 +1,32 @@
+/*global define*/
+
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'templates'
+], function ($, _, Backbone, JST) {
+    'use strict';
+
+    var FeaturedconnectionView = Backbone.View.extend({
+        template: JST['app/scripts/templates/featuredConnection.ejs'],
+
+        tagName: 'div',
+
+        id: '',
+
+        className: '',
+
+        events: {},
+
+        initialize: function () {
+            this.listenTo(this.model, 'change', this.render);
+        },
+
+        render: function () {
+            this.$el.html(this.template(this.model.toJSON()));
+        }
+    });
+
+    return FeaturedconnectionView;
+});
---
+++ app/scripts/views/featuredPerson.js
@@ -0,0 +1,32 @@
+/*global define*/
+
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'templates'
+], function ($, _, Backbone, JST) {
+    'use strict';
+
+    var FeaturedpersonView = Backbone.View.extend({
+        template: JST['app/scripts/templates/featuredPerson.ejs'],
+
+        tagName: 'div',
+
+        id: '',
+
+        className: '',
+
+        events: {},
+
+        initialize: function () {
+            this.listenTo(this.model, 'change', this.render);
+        },
+
+        render: function () {
+            this.$el.html(this.template(this.model.toJSON()));
+        }
+    });
+
+    return FeaturedpersonView;
+});
