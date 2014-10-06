239d8c8ef7a5e2ee7ab0b06aad255bff2f470dd3
anabranch <wac.chambers@gmail.com>
2014-10-05 22:56:55 -0700

    Testing out who else shows up, removed log statement

--- app/scripts/views/featuredPerson.js
+++ app/scripts/views/featuredPerson.js
@@ -21,7 +21,6 @@ define([
         render: function () {
             if (this.model.get('featured')) {
                 this.$el.html(this.template(this.model.toJSON()));
-                console.log('hello');
             }
         }
     });
